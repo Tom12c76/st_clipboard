cfff77cdaef9eccd24dfc77141c036e492bea915
Tom12c76 <thomascastri@gmail.com>
2021-11-01 11:11:54 +0100

    button loop

--- clipboard.py
+++ clipboard.py
@@ -7,15 +7,16 @@ todo = st.radio('what do you want to do', ['save clipboard data to csv', 'downlo
 
 if todo == 'save clipboard data to csv':
     if st.button('read clipboard & save csv'):
-        try:
-            df = pd.read_clipboard()
-            st.write(df)
-            now = datetime.now()
-            file_name = f'{now:%Y-%m-%d %H-%M-%S} clipboard data.csv'
-            df.to_csv(file_name, index=False)
-            st.success(f'file has been saved with {file_name} name')
-        except:
-            st.error('clipboard empty or error with data')
+        st.success('button pressed')
+        # try:
+        df = pd.read_clipboard()
+        st.write(df)
+        now = datetime.now()
+        file_name = f'{now:%Y-%m-%d %H-%M-%S} clipboard data.csv'
+        df.to_csv(file_name, index=False)
+        st.success(f'file has been saved with {file_name} name')
+        # except:
+        #     st.error('clipboard empty or error with data')
 
 if todo == 'download csv files':
     list_csv = sorted(glob.glob('*.csv'), reverse=True)
