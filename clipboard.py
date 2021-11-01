36b76da17e4217a6019fe1715edc6a710cf45ba0
Tom12c76 <thomascastri@gmail.com>
2021-11-01 11:03:17 +0100

    Initial commit

---
+++ clipboard.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import streamlit as st
+from datetime import datetime
+import glob
+
+todo = st.radio('what do you want to do', ['save clipboard data to csv', 'download csv files'])
+
+if todo == 'save clipboard to csv':
+    if st.button('read clipboard & save csv'):
+        try:
+            df = pd.read_clipboard()
+            st.write(df)
+            now = datetime.now()
+            file_name = f'{now:%Y-%m-%d %H-%M-%S} clipboard data.csv'
+            df.to_csv(file_name, index=False)
+            st.success(f'file has been saved with {file_name} name')
+        except:
+            st.error('clipboard empty or error with data')
+
+if todo == 'download csv files':
+    list_csv = sorted(glob.glob('*.csv'), reverse=True)
+    d_filename = st.selectbox('Choose a file', options=list_csv)
+    df = pd.read_csv(d_filename)
+    st.write(df)
+    st.download_button('dwnload now', data=df.to_csv(index=False), file_name=d_filename, mime='text/csv')
